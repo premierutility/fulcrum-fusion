27101e35f6073d3847c41eaef99548fe7f7606b2
Kyle Tolle <kyle@nullsix.com>
2013-10-04 12:20:09 -0600

    Create a RecordProcessor class/file

---
+++ record_processor.rb
@@ -0,0 +1,37 @@
+class RecordProcessor
+  def process(action, event_data)
+    id = event_data["data"]["form_id"].gsub("-", "")
+    f = FulcrumTable.new.existing_table(id)
+    record_row = convert_record_data(event_data["data"])
+
+    return unless f
+
+    case action
+    when 'create'
+      f.insert([record_row])
+    when 'update'
+      row = f.select("ROWID", "WHERE id='#{record_row["id"]}'").first
+      row_id = row ? row[:rowid] : nil
+      if row_id
+        f.update(row_id, record_row)
+      end
+    when 'delete'
+      row = f.select("ROWID", "WHERE id='#{record_row["id"]}'").first
+      row_id = row ? row[:rowid] : nil
+      if row_id
+        f.delete(row_id)
+      end
+    end
+  rescue => e
+    puts "ERROR: #{e.inspect}, #{e.backtrace}"
+  end
+
+private
+  def convert_record_data(record)
+    record["form_values"] = record["form_values"].to_json
+    location = "#{record.delete('latitude')},#{record.delete('longitude')}"
+    record['location'] = location
+    record
+  end
+end
+
