fc62e06c3e48dfd1b228b93e8dad1df8538ac875
Kyle Tolle <kyle@nullsix.com>
2013-10-21 11:09:49 -0600

    Ensure dup record.create events aren't processed

--- lib/record_processor/record_creator.rb
+++ lib/record_processor/record_creator.rb
@@ -1,8 +1,13 @@
 class RecordProcessor
   class RecordCreator < Base
     def process
+      return if row_already_exists?
       @table.insert([@record_row])
       201
+    end
+
+    def row_already_exists?
+      row
     end
   end
 end
