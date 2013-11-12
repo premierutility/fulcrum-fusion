aa362dbcde834fd0d5d46a719b3f08244a902012
Kyle Tolle <kyle@nullsix.com>
2013-11-12 14:36:48 -0700

    Make the sinatra app modular
    Allows us to start the app with Rack.
    Also add in lines to use Bundler.

---
+++ config.ru
@@ -0,0 +1,8 @@
+require 'rubygems'
+require 'bundler'
+
+Bundler.require
+
+require './fulcrum_fusion'
+run FulcrumFusion
+
--- fulcrum_fusion.rb
+++ fulcrum_fusion.rb
@@ -1,32 +1,39 @@
-require 'sinatra'
-require 'json'
-require_relative 'lib/status'
-require_relative 'lib/event_data'
-require_relative 'lib/event_processor'
-require_relative 'config.rb' if File.exists?('config.rb')
+require 'sinatra/base'
 
-set :port, ENV['PORT'] if ENV['PORT']  # Configure sinatra
+class FulcrumFusion < Sinatra::Base
+  require 'json'
+  require_relative 'lib/status'
+  require_relative 'lib/event_data'
+  require_relative 'lib/event_processor'
+  require_relative 'config.rb' if File.exists?('config.rb')
 
-get '/' do
-  "Fulcrum Fusion is running"
-end
+  #TODO: Remove this here and use rackup -p 3002
+  set :port, ENV['PORT'] if ENV['PORT']  # Configure sinatra
 
-post '/' do
-  request.body.rewind
-  post_body = request.body.read
-
-  begin
-    event_data = EventData.new(post_body).read
-
-  rescue TypeError, EventData::BadDataError
-    return Status::BAD_REQUEST
+  get '/' do
+    "Fulcrum Fusion is running"
   end
 
-  begin
-    EventProcessor.new(event_data).process
+  post '/' do
+    request.body.rewind
+    post_body = request.body.read
 
-  rescue TypeError, ArgumentError
-    return Status::BAD_REQUEST
+    begin
+      event_data = EventData.new(post_body).read
+
+    rescue TypeError, EventData::BadDataError
+      return Status::BAD_REQUEST
+    end
+
+    begin
+      EventProcessor.new(event_data).process
+
+    rescue TypeError, ArgumentError
+      return Status::BAD_REQUEST
+    end
   end
+
+  # Start the server if ruby executed the file directly
+  run! if app_file == $0
 end
 
