0fc6e103269154fd738398944f31ce203c4b9abe
Kyle Tolle <kyle@nullsix.com>
2013-11-14 13:06:01 -0700

    Check in RecordData text field spec
    Guess I forgot to add this in the pervious commit. My apologies, git.

--- spec/record_data_spec.rb
+++ spec/record_data_spec.rb
@@ -22,17 +22,81 @@ describe RecordData do
   end
 
   describe "#fusion_format" do
+    describe "with an address field"
+    describe "with a choice field"
+    describe "with a classification field"
+    describe "with a datetime field"
+    describe "with a label"
+    describe "with a photo field"
+    describe "with a section"
+    describe "with a signature field"
+
     describe "with a text field" do
-      let(:expected_fusion_format_with_text_field) do
+      let(:expected_fusion_format) do
         { 'Name' => 'Fake Record' }.
           merge(expected_raw_format)
       end
 
+      let(:text_record) { record_data }
+
       it "converts the data properly" do
-        Form.any_instance.stub(:field_key_name_mappings).
-          and_return({'94f8' => 'Name'})
-        actual_fusion_format = RecordData.new(record_data).fusion_format
-        actual_fusion_format.should == expected_fusion_format_with_text_field
+        Form.any_instance.stub(:find_field_by_key).with('94f8').
+          and_return(
+            {
+              "type" => "TextField",
+              "key" => "94f8",
+              "label" => "Name",
+              "description" => nil,
+              "required" => false,
+              "disabled" => false,
+              "hidden" => false,
+              "data_name" => "name",
+              "default_value" => nil,
+              "visible_conditions_type" => nil,
+              "visible_conditions" => nil,
+              "required_conditions_type" => nil,
+              "required_conditions" => nil,
+              "numeric" => false
+            }
+          )
+        actual_fusion_format = RecordData.new(text_record).fusion_format
+        actual_fusion_format.should == expected_fusion_format
+      end
+    end
+
+    describe "with a numeric field" do
+      let(:expected_fusion_format) do
+        { 'Number' => 100 }.
+          merge(expected_raw_format).
+          merge({'form_values' => "{\"94f8\":\"100\"}"})
+      end
+
+      let(:numeric_record) do
+        record_data.merge( {'form_values' => { '94f8' => '100' } } )
+      end
+
+      it "converts the data properly" do
+        Form.any_instance.stub(:find_field_by_key).with('94f8').
+          and_return(
+            {
+              "type" => "TextField",
+              "key" => "94f8",
+              "label" => "Number",
+              "description" => nil,
+              "required" => false,
+              "disabled" => false,
+              "hidden" => false,
+              "data_name" => "name",
+              "default_value" => nil,
+              "visible_conditions_type" => nil,
+              "visible_conditions" => nil,
+              "required_conditions_type" => nil,
+              "required_conditions" => nil,
+              "numeric" => true
+            }
+          )
+        actual_fusion_format = RecordData.new(numeric_record).fusion_format
+        actual_fusion_format.should == expected_fusion_format
       end
     end
   end
