6f3527513bec1ae1a5be1841eedbc482065a2713
Kyle Tolle <kyle@nullsix.com>
2013-11-14 16:33:33 -0700

    Refactor RecordData extracting
    Break multiple responsibilities into smaller classes.

--- lib/record_data/extractor.rb
+++ lib/record_data/extractor.rb
@@ -23,30 +23,26 @@ class RecordData
     class NumericField < TextField; end
   end
 
-  class Extractor
-    def initialize(unconverted_data)
-      @unconverted_data = unconverted_data
+  class ExtractorForField
+    def initialize(field_type, numeric)
+      @field_type = field_type
+      @numeric    = numeric
     end
 
-    def extract
-      form = Form.new(@unconverted_data['form_id'])
-      form_values = JSON.parse(@unconverted_data['form_values'])
-      extracted_record_data = {}
+    def extractor_class
+      if numeric_field?
+        EXTRACTORS['NumericField']
 
-      form_values.each do |form_field_key, form_field_value|
-        form_field = form.find_field_by_key(form_field_key)
-        return unless form_field
-        field_name = form_field['label']
-        field_type = form_field['type']
-        extractor_class = EXTRACTORS[field_type]
-        extracted_data = extractor_class.new(form_field_value).extract
-        extracted_record_data[field_name] = extracted_data
+      else
+        EXTRACTORS[@field_type]
       end
-
-      extracted_record_data
     end
 
   private
+    def numeric_field?
+      @field_type == 'TextField' && @numeric
+    end
+
     EXTRACTORS =
       {
         'AddressField'        => RecordData::Extractors::AddressField,
@@ -60,5 +56,66 @@ class RecordData
         'TextField'           => RecordData::Extractors::TextField,
         'NumericField'        => RecordData::Extractors::NumericField
       }.freeze
+  end
+
+  class Field
+    def initialize(form, field_key, field_value)
+      @form = form
+      @field_key = field_key
+      @field_value = field_value
+    end
+
+    def extracted_data
+      return unless form_field
+
+      extractor_class.new(@field_value).extract
+    end
+
+    def name
+      form_field['label']
+    end
+
+  private
+    def form_field
+      unless @form_field
+        @form_field = @form.find_field_by_key(@field_key)
+      end
+
+      @form_field
+    end
+
+    def type
+      form_field['type']
+    end
+
+    def extractor_class
+      ExtractorForField.new(type, form_field['numeric']).extractor_class
+    end
+  end
+
+  class Extractor
+    def initialize(unconverted_data)
+      @unconverted_data = unconverted_data
+    end
+
+    def extract
+      extracted_record_data = {}
+
+      form_values.each do |field_key, field_value|
+        field = Field.new(form, field_key, field_value)
+        extracted_record_data[field.name] = field.extracted_data
+      end
+
+      extracted_record_data
+    end
+
+  private
+    def form
+      Form.new(@unconverted_data['form_id'])
+    end
+
+    def form_values
+      JSON.parse(@unconverted_data['form_values'])
+    end
   end
 end
