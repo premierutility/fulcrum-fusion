68798d7ea64e03afae7a20baabeb6409706f8b0a
Kyle Tolle <kyle@nullsix.com>
2013-11-15 15:51:45 -0700

    Handle inserting SignatureField data in records

--- lib/record_data/extractors/signature_field.rb
+++ lib/record_data/extractors/signature_field.rb
@@ -1,6 +1,11 @@
+require_relative 'base'
+
 class RecordData
   module Extractors
-    class SignatureField
+    class SignatureField < Base
+      def extract
+        @field_value['url']
+      end
     end
   end
 end
